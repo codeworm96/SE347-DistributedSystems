86658fb4956e3cc6c3f0e1f40df7cdb72f998cf7
codeworm96 <codeworm96@outlook.com>
2017-03-05 22:31:31 +0800

    Selective Repeat

--- rdt/rdt_receiver.cc
+++ rdt/rdt_receiver.cc
@@ -18,17 +18,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <deque>
+
 #include "rdt_struct.h"
 #include "rdt_receiver.h"
 #include "rdt_common.h"
 
 static unsigned int expect_seq;
+static std::deque<packet *> window;
 
 /* receiver initialization, called once at the very beginning */
 void Receiver_Init()
 {
     fprintf(stdout, "At %.2fs: receiver initializing ...\n", GetSimulationTime());
     expect_seq = 1;
+    window.clear();
+    window.resize(window_size);
 }
 
 /* receiver finalization, called once at the very end.
@@ -47,23 +52,35 @@ void Receiver_FromLowerLayer(struct packet *pkt)
     if (crc32(pkt->data + 4, RDT_PKTSIZE - 4) == *(unsigned int *)pkt->data) { // ignore corrupted packets
         unsigned int seq = *(unsigned int *)(pkt->data + 4);
         fprintf(stdout, "received %d, expect %d\n", seq, expect_seq);
-        if (seq == expect_seq) { // Go Back To N
-            ++expect_seq;
-            /* construct a message and deliver to the upper layer */
-            message *msg = (message *)malloc(sizeof(message));
-            ASSERT(msg!=NULL);
-            msg->size = pkt->data[8];
-            msg->data = (char *)malloc(sizeof(char) * msg->size);
-            ASSERT(msg->data!=NULL);
-            memcpy(msg->data, pkt->data+header_size, msg->size);
-            Receiver_ToUpperLayer(msg);
-            /* don't forget to free the space */
-            if (msg->data!=NULL) free(msg->data);
-            if (msg!=NULL) free(msg);
+        if (seq >= expect_seq && seq - expect_seq < window_size) { // select repeat
+            if (window[seq - expect_seq] == NULL) {
+                packet *p = (packet *)malloc(sizeof(packet));
+                *p = *pkt;
+                window[seq - expect_seq] = p;
+            }
+            while (window.front()) {
+                ++expect_seq;
+                packet *p = window.front();
+                window.pop_front();
+                window.push_back(NULL);
+
+                /* construct a message and deliver to the upper layer */
+                message *msg = (message *)malloc(sizeof(message));
+                ASSERT(msg!=NULL);
+                msg->size = p->data[8];
+                msg->data = (char *)malloc(sizeof(char) * msg->size);
+                ASSERT(msg->data!=NULL);
+                memcpy(msg->data, p->data+header_size, msg->size);
+                Receiver_ToUpperLayer(msg);
+                /* don't forget to free the space */
+                free(p);
+                if (msg->data!=NULL) free(msg->data);
+                if (msg!=NULL) free(msg);
+            }
             /* ack */
             packet *ack = (packet *)malloc(sizeof(packet));
             memset(ack, 0, sizeof(packet));
-            *(unsigned int *)(ack->data + 4) = seq;
+            *(unsigned int *)(ack->data + 4) = expect_seq - 1;
             unsigned int crc = crc32(ack->data + 4, RDT_PKTSIZE - 4);
             *(unsigned int *)ack->data = crc;
             Receiver_ToLowerLayer(ack);
